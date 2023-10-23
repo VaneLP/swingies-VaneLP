f3c67bb7db9165134c0a75014194cd565292e014
Vane <123445952+VaneLP@users.noreply.github.com>
2023-10-23 10:21:31 +0200

    Terminado dia 1

--- src/Menu.java
+++ src/Menu.java
@@ -1,43 +1,43 @@
 import javax.swing.*;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
-import javax.swing.event.MenuKeyEvent;
-import javax.swing.event.MenuKeyListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Menu extends JFrame {
+    //atributos
     private JTabbedPane tabbedPane1;
     private JPanel panelPrincipal;
     private JTabbedPane prof;
     private JTabbedPane cur;
     private JTabbedPane alum;
 
-    private JMenu menu, menuA, menuP;
+    //atributos menu
+    private JMenu menu, menuA, menuP, menuC;
     private JMenuBar menuBar;
     private JLabel icono;
-    private JLabel prueba;
     private JMenuItem o1, o2, o3, o4;
 
     public Menu() {
+        //para que se muestre en la ventana nuestro panel
         setContentPane(panelPrincipal);
 
+        //cerrar cuando pulsamos la X
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        //el tamaño de la ventana
         setSize(700, 500);
 
+        //para que se centre en medio de la pantalla
         setLocationRelativeTo(null);
 
+        //para hacer visible la ventana
         setVisible(true);
 
+//-------------------------------------------------------------------------------------------------------------
         // ---- MENU ----
-
         //asignamos el JMenuBAr
         setJMenuBar(menuBar);
 
+//-------------------------------------------------------------------------------------------------------------
         // ---- ALUMNOS ----
         //creamos un nuevo menu y le ponemos un titulo
         menuA = new JMenu("Alumnos");
@@ -56,30 +56,10 @@ public class Menu extends JFrame {
         menuA.add(o3);
         menuA.add(o4);
 
-
-        //PRUEBAS 2
+        // al menu de alumnos le asociamos el atajo de Alt+A
         menuA.setMnemonic(KeyEvent.VK_A);
 
-        menuA.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                prueba.setText("FUNCIONA NEMO ALUM");
-            }
-        });
-
-
-        //TODO cambiar todo
-        //pruebas
-//        o1.setMnemonic(KeyEvent.VK_A);
-//
-//        o1.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                prueba.setText("FUNCIONO");
-//            }
-//        });
-
-
+//-------------------------------------------------------------------------------------------------------------
         // ---- PROFESORES ----
         //creamos un nuevo menu y le ponemos un titulo
         menuP = new JMenu("Profesores");
@@ -98,39 +78,32 @@ public class Menu extends JFrame {
         menuP.add(o3);
         menuP.add(o4);
 
-
-        //PRUEBAS 2
+        // al menu de profesores le asociamos el atajo de Alt+P
         menuP.setMnemonic(KeyEvent.VK_P);
 
-        menuP.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                prueba.setText("FUNCIONA NEMO PROF");
-            }
-        });
-
-
-
-
-
+//-------------------------------------------------------------------------------------------------------------
         // ---- CURSO ----
         //creamos un nuevo menu y le ponemos un titulo
-        menu = new JMenu("Curso");
+        menuC = new JMenu("Curso");
         //a nuestro JMenuBar le asignamos nuestro menu
-        menuBar.add(menu);
+        menuBar.add(menuC);
 
         //creamos los items y le pomenos un nombre
-        o1 = new JMenuItem("Agregar"+"      "+"c+1");
-        o2 = new JMenuItem("Buscar"+"      "+"c+2");
-        o3 = new JMenuItem("Eliminar"+"      "+"c+3");
-        o4 = new JMenuItem("Mostrar"+"      "+"c+4");
+        o1 = new JMenuItem("Agregar");
+        o2 = new JMenuItem("Buscar");
+        o3 = new JMenuItem("Eliminar");
+        o4 = new JMenuItem("Mostrar");
 
         //añadimos a nuestro menu los items que acabamos de crear
-        menu.add(o1);
-        menu.add(o2);
-        menu.add(o3);
-        menu.add(o4);
+        menuC.add(o1);
+        menuC.add(o2);
+        menuC.add(o3);
+        menuC.add(o4);
 
+        // al menu de curso le asociamos el atajo de Alt+C
+        menuC.setMnemonic(KeyEvent.VK_C);
+
+//-------------------------------------------------------------------------------------------------------------
         // ---- ABOUT ----
         // creamos un nuevo menu y le ponemos un titulo
         menu = new JMenu("About");
@@ -143,6 +116,7 @@ public class Menu extends JFrame {
         //añadimos a nuestro menu los items que acabamos de crear
         menu.add(o1);
 
+//-------------------------------------------------------------------------------------------------------------
         //agregamos una imagen
         icono.setIcon(new ImageIcon("src/imagenes/logoThiar.png"));
 
@@ -150,6 +124,11 @@ public class Menu extends JFrame {
     }
 
 
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------
     // ---- MAIN ----
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
