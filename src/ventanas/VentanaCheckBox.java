d660d71bce1156c1f84f9a9d251cf5e600027d40
Vane <vanessa-lopez9@hotmail.com>
2024-01-20 13:16:14 +0100

    .

--- src/ventanas/VentanaCheckBox.java
+++ src/ventanas/VentanaCheckBox.java
@@ -1,8 +1,18 @@
 package ventanas;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaCheckBox extends JDialog{
     private JPanel panelPrincipal;
     private JButton button1;
+public VentanaCheckBox() {
+    button1.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+    });
+}
 }
